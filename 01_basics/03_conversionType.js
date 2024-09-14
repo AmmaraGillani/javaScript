10b284b8ec63fd19cacf4374ccc133c02b7f784d
AmmaraGillani <126082977+AmmaraGillani@users.noreply.github.com>
2024-09-14 13:02:13 +0000

    numberConversion

---
+++ 01_basics/03_conversionType.js
@@ -0,0 +1,34 @@
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1 or false => 0
+// null => 0
+// undefined => undefined
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true or 0 => false
+// "" => false
+// "ammara" => true 
+
+
+let someNumber = 33
+console.log(typeof someNumber);
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
